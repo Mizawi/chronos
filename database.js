dd9ee8bbecd71490699006e762093c87e6ebf034
Mizawi <fc48314@alunos.fc.ul.pt>
2019-04-22 20:54:44 +0100

    updated gitignore

--- database.js
+++ database.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 //II        UNCOMMENT TO DEPLOY TO GCLOUD/COMMENT FOR LOCAL DEVELOPMENT       II//
 //II==========================================================================II//
 
-/*
 let config = {
 
     user: process.env.SQL_USER,
@@ -26,7 +25,7 @@ if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production
 }
 
 let connection = mysql.createConnection(config);
-*/
+
 //************************************************************************************//
 //************************************************************************************//
 
@@ -34,6 +33,7 @@ let connection = mysql.createConnection(config);
 //II        UNCOMMENT FOR LOCAL DEVELOPMENT/COMMENT TO DEPLOY TO GCLOUD       II//
 //II==========================================================================II//
 
+/*
 // Database Connection for Development
 
 let connection = mysql.createConnection({
@@ -58,4 +58,4 @@ con = connection.connect(function(err) {
 });
 
 
-module.exports = connection;
+module.exports = connection; */
