801eb9567d77773aef43a3faf5f86ba531baecda
Mizawi <fc48314@alunos.fc.ul.pt>
2019-04-24 10:14:35 +0100

    profile page done

--- views/js/student.js
+++ views/js/student.js
@@ -41,14 +41,33 @@ jQuery(function($) {
             type: "get",
             dataType: "json",
             success: (data) => {
-                $('#query_table_profile_answer').remove();
+                $('.container-profile').remove();
                 var info = JSON.parse(data[0].information)
-                content = '<div class="show container-profile"> <div class="container-profile-icon"> <h5>Informação Principal</h5><br> <div class="container-profile-icon-image"><img src="images/user.jpg" alt="User picture"> </div><div class="container-profile-icon-info"><p>Nome:' +
+                content = '<div class="show container-profile"> ++<div class="container-profile-icon"> <h5>Informação Principal</h5><br> <div class="container-profile-icon-image"><img src="images/user.jpg" alt="User picture"> </div><div class="container-profile-icon-info"><p>Nome:' +
                     info.nome + '</p><p>Cargo:' +
                     info.cargo + '</p><p>Numero:' +
                     info.numero + '</p><p>Email:' +
-                    data[0].email + '</p></div>';
-                $('#query_table_profile').replace(content);
+                    data[0].email + '</p></div></div><div class="container-profile-maininfo"><h5> Contactos e Disponibilização de Informação </h4><br><p> Nome de Utilizador: ' +
+                    info.nomeUtilizador + '</p><p> Morada[Pessoal]: ' +
+                    info.morada + ' </p><p> Email[Pessoal]: ' +
+                    info.emailp + ' </p><p> Email[Institucional]: ' +
+                    data[0].email + '</p></div><div class="container-profile-personaldata"><h5>Dados Pessoais</h5><br><p> Nomes Próprios: ' +
+                    info.nome.split(" ")[0] + ' ' + info.nome.split(" ")[1] + ' </p><p> Apelidos: ' +
+                    info.nome.split(" ")[info.nome.split(" ").length - 1] + ' </p><p> Sexo: ' +
+                    info.sexo + ' </p><p> Nº Documento de Identificação: ' +
+                    info.documentoDeIdentificacao + ' </p><p> Local de Emissão: ' +
+                    info.localdeEmissao + ' </p><p> Emitido em: ' +
+                    info.emitidoEm + ' </p><p> Válido até: ' +
+                    info.valido + '</p><p> Nº de Contribuinte: ' +
+                    info.contribuinte + '</p><p> Profissão: ' +
+                    info.profissao + ' </p><p> Estado Civil: ' +
+                    info.estadoCivil + ' </p><p> Data de Nascimento: ' +
+                    info.dataNascimento + ' </p><p> Nacionalidade: ' +
+                    info.nacionalidade + ' </p><p> Freguesia de Nascimento: ' +
+                    info.freguesiaNascimento + ' </p><p> Concelho de Nascimento: ' +
+                    info.concelhoNascimento + ' </p><p> Distrito de Nascimento: ' +
+                    info.distritoNascimento + '</p></div></div>'
+                $('#query_table_profile').append(content);
 
             }
         });
