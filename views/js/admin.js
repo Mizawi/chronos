0e06358ce07c210c6c48c90094817302a36efec9
miguelacorreia <miguel.a.correia@aitec.pt>
2019-05-11 19:22:15 +0100

    Fix all admin shows, fix student search bar

--- views/js/admin.js
+++ views/js/admin.js
@@ -30,20 +30,20 @@ jQuery(function($) {
     });
 
     $("#searchAll").click(() => {
+        $(".search-admin-bar").hide();
         $.ajax({
             url: "/searchAll",
             type: "get",
             dataType: "json",
             success: (data) => {
                 $('#query-table-answer').remove();
-                var text = "";
                 var content = "<table class='table table-sm table-dark' id='query-table-answer'> <thead class='table-info' > <tr><th>Name </th> <th>Email</th></tr></thead>"
-                for (key in data) {                    
-                    for (item in data[key]) {
-                        var info = JSON.parse(item.information);
-                        content += '<tr><td>' + info.nome  + '</td>' + '<td>' + data[key][item].email + '<td>' + '</tr>'
-                    }
-                }
+                data.forEach(datai => {
+                    datai.forEach(item => {
+                        const info = JSON.parse(item.information);
+                        content += '<tr><td>' + info.nome  + '</td>' + '<td>' + item.email + '<td>' + '</tr>'
+                    })
+                });
                 content += "</table>"
                 $('#query_table').append(content);
             }
@@ -51,17 +51,18 @@ jQuery(function($) {
     });
 
     $("#searchStudents").click(() => {
+        $(".search-admin-bar").show();
         $.ajax({
             url: "/searchStudent",
             type: "get",
             dataType: "json",
             success: (data) => {
                 $('#query-table-answer').remove();
-                var text = "";
                 var content = "<table class='table table-sm table-dark' id='query-table-answer'> <thead class='table-info' > <tr><th scope='col'>Name </th> <th scope='col'> Email </th></tr></thead>"
-                for (key in data) {
-                    content += '<tr><td>' + data[key].name  + '</td>' + '<td>' + data[key].email + '<td>' + '</tr>'
-                }
+                data.forEach(item => {
+                    const info = JSON.parse(item.information);
+                    content += '<tr><td>' + info.nome  + '</td>' + '<td>' + item.email + '<td>' + '</tr>'
+                });
                 content += "</table>"
                 $('#query_table').append(content);
             }
@@ -69,17 +70,18 @@ jQuery(function($) {
     });
 
     $("#searchTeachers").click(() => {
+        $(".search-admin-bar").hide();
         $.ajax({
             url: "/searchTeacher",
             type: "get",
             dataType: "json",
             success: (data) => {
                 $('#query-table-answer').remove();
-                var text = "";
                 var content = "<table class='table table-sm table-dark' id='query-table-answer'> <thead class='table-info' > <tr><th scope='col'>Name </th> <th scope='col'> Email </th></tr></thead>"
-                for (key in data) {
-                    content += '<tr><td>' + data[key].name  + '</td>' + '<td>' + data[key].email + '<td>' + '</tr>'
-                }
+                data.forEach(item => {
+                    const info = JSON.parse(item.information);
+                    content += '<tr><td>' + info.nome  + '</td>' + '<td>' + item.email + '<td>' + '</tr>'
+                });
                 content += "</table>"
                 $('#query_table').append(content);
             }
@@ -87,9 +89,7 @@ jQuery(function($) {
     });
 
     $("#btnBusca").click(() => {
-
         valor_a_pesquisar = document.getElementById("txtBusca").value
-
         $.ajax({
             url: "/searchStudentsByEmailOrNumber",
             type: "get",
@@ -97,27 +97,33 @@ jQuery(function($) {
             data: {texto_pesquisa:valor_a_pesquisar},
             success: (data) => {
                 $('#query-table-answer').remove();
-                var text = "";
-                var info = JSON.parse(data[0].information);
-                var content = "<table class='table table-sm table-dark' id='query-table-answer'> <thead class='table-info' > <tr><th scope='col'>INFORMAÇAO </th></tr></thead>";
-                console.log(info);
-                content += "<thead class='table-info' > <tr><th scope='col'>Nome </th></tr></thead>"
-                content += '<tr><td>' + info.nome + '<td></tr>';
-                content += "<thead class='table-info' > <tr><th scope='col'>Data Nascimento </th></tr></thead>"
-                content += '<tr><td>' + info.dataNascimento + '<td></tr>';
-                content += "<thead class='table-info' > <tr><th scope='col'>Documento Identificação </th></tr></thead>"
-                content += '<tr><td>' + info.documentoDeIdentificacao + '<td></tr>';
-                content += "<thead class='table-info' > <tr><th scope='col'>Numero </th></tr></thead>"
-                content += '<tr><td>' + info.numero + '<td></tr>';
-                content += "<thead class='table-info' > <tr><th scope='col'>Cargo </th></tr></thead>"
-                content += '<tr><td>' + info.cargo + '<td></tr>';
-                content += "</table>";
-                $('#query_table').append(content);
+                if(data[0]){
+                    var info = JSON.parse(data[0].information);
+                    var content = "<table class='table table-sm table-dark' id='query-table-answer'> <thead class='table-info' > <tr><th scope='col'>INFORMAÇAO </th></tr></thead>";
+                    content += "<thead class='table-info' > <tr><th scope='col'>Nome </th></tr></thead>"
+                    content += '<tr><td>' + info.nome + '<td></tr>';
+                    content += "<thead class='table-info' > <tr><th scope='col'>Data Nascimento </th></tr></thead>"
+                    content += '<tr><td>' + info.dataNascimento + '<td></tr>';
+                    content += "<thead class='table-info' > <tr><th scope='col'>Documento Identificação </th></tr></thead>"
+                    content += '<tr><td>' + info.documentoDeIdentificacao + '<td></tr>';
+                    content += "<thead class='table-info' > <tr><th scope='col'>Numero </th></tr></thead>"
+                    content += '<tr><td>' + info.numero + '<td></tr>';
+                    content += "<thead class='table-info' > <tr><th scope='col'>Cargo </th></tr></thead>"
+                    content += '<tr><td>' + info.cargo + '<td></tr>';
+                    content += "</table>";
+                    $('#query_table').append(content);
+                }else{
+                    var content = "<table class='table table-sm table-dark' id='query-table-answer'> <thead class='table-info' > <tr><th scope='col'>INFORMAÇAO </th></tr></thead>";
+                    content += '<tr><td>' + "Não existe nenhum aluno que corresponda a esta pesquisa" + '<td></tr>';
+                    content += "</table>";
+                    $('#query_table').append(content);
+                }
             }
         })
     });
 
     $("#getLogs").click(() => {
+        $(".search-admin-bar").show();
         $.ajax({
             url: "/getLogs",
             type: "get",
